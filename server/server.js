f3eaaade2a4c2691852b10c581b321f7dbf27233
BarGar <bargar54400@gmail.com>
2026-04-08 19:44:33 -0500

    add server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("send-message", (msg) => {
+    io.emit("receive-message", msg);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server running"));
