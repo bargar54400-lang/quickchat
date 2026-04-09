81674c7bfbf7792629338d5ee56d9744798aa535
BarGar <bargar54400@gmail.com>
2026-04-08 20:18:21 -0500

    upgrade to pro

--- client/app.js
+++ client/app.js
@@ -1,17 +1,50 @@
-const socket = io("https://quickchat-server-hap7.onrender.com/");
+const socket = io("https://your-render-url.onrender.com");
 
-const input = document.getElementById("messageInput");
-const messages = document.getElementById("messages");
+let username = "";
+let selectedUser = null;
 
-input.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    socket.emit("send-message", input.value);
-    input.value = "";
+function join() {
+  username = document.getElementById("username").value;
+  socket.emit("join", username);
+}
+
+// Send message
+document.getElementById("messageInput").addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && selectedUser) {
+    socket.emit("private-message", {
+      to: selectedUser,
+      message: e.target.value
+    });
+    e.target.value = "";
+  } else {
+    socket.emit("typing", selectedUser);
   }
 });
 
-socket.on("receive-message", (msg) => {
+// Receive message
+socket.on("receive-message", (data) => {
   const div = document.createElement("div");
-  div.innerText = msg;
-  messages.appendChild(div);
+  div.innerText = `${data.from}: ${data.message}`;
+  document.getElementById("messages").appendChild(div);
+});
+
+// Typing
+socket.on("typing", (user) => {
+  document.getElementById("typing").innerText = user + " is typing...";
+  setTimeout(() => {
+    document.getElementById("typing").innerText = "";
+  }, 1000);
+});
+
+// Online users
+socket.on("online-users", (users) => {
+  const list = document.getElementById("users");
+  list.innerHTML = "";
+
+  Object.keys(users).forEach(id => {
+    const li = document.createElement("div");
+    li.innerText = users[id];
+    li.onclick = () => selectedUser = id;
+    list.appendChild(li);
+  });
 });
--- server/server.js
+++ server/server.js
@@ -6,23 +6,42 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("QuickChat server is running 🚀");
-});
-
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: { origin: "*" }
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+let users = {};
 
-  socket.on("send-message", (msg) => {
-    io.emit("receive-message", msg);
+app.get("/", (req, res) => {
+  res.send("QuickChat Pro Running 🚀");
+});
+
+io.on("connection", (socket) => {
+
+  socket.on("join", (username) => {
+    users[socket.id] = username;
+    io.emit("online-users", users);
   });
+
+  socket.on("private-message", ({ to, message }) => {
+    socket.to(to).emit("receive-message", {
+      message,
+      from: users[socket.id]
+    });
+  });
+
+  socket.on("typing", (to) => {
+    socket.to(to).emit("typing", users[socket.id]);
+  });
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("online-users", users);
+  });
+
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server running on " + PORT));
+server.listen(PORT, () => console.log("Server running"));
