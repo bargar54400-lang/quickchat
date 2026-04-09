66babcbe743c243f03a32b68a257e023fd5b00fe
BarGar <bargar54400@gmail.com>
2026-04-08 19:54:43 -0500

    fix server

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("QuickChat server is running 🚀");
+});
+
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: { origin: "*" }
 });
@@ -20,4 +25,4 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server running"));
+server.listen(PORT, () => console.log("Server running on " + PORT));
