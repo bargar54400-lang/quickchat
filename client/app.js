c9e31da4157233a77e4473f9f3ce255d2617a49c
BarGar <bargar54400@gmail.com>
2026-04-08 20:11:38 -0500

    add frontend

---
+++ client/app.js
@@ -0,0 +1,17 @@
+const socket = io("https://quickchat-server-hap7.onrender.com/");
+
+const input = document.getElementById("messageInput");
+const messages = document.getElementById("messages");
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    socket.emit("send-message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("receive-message", (msg) => {
+  const div = document.createElement("div");
+  div.innerText = msg;
+  messages.appendChild(div);
+});
