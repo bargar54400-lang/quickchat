67d8be57a2c4dcfa8d9a46b2a335ee8d07c0eb17
BarGar <bargar54400@gmail.com>
2026-04-08 20:30:55 -0500

    fix css

--- client/app.js
+++ client/app.js
@@ -1,50 +1,17 @@
 const socket = io("https://quickchat-server-hap7.onrender.com");
 
-let username = "";
-let selectedUser = null;
+const input = document.getElementById("messageInput");
+const messages = document.getElementById("messages");
 
-function join() {
-  username = document.getElementById("username").value;
-  socket.emit("join", username);
-}
-
-// Send message
-document.getElementById("messageInput").addEventListener("keypress", (e) => {
-  if (e.key === "Enter" && selectedUser) {
-    socket.emit("private-message", {
-      to: selectedUser,
-      message: e.target.value
-    });
-    e.target.value = "";
-  } else {
-    socket.emit("typing", selectedUser);
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    socket.emit("send-message", input.value);
+    input.value = "";
   }
 });
 
-// Receive message
-socket.on("receive-message", (data) => {
+socket.on("receive-message", (msg) => {
   const div = document.createElement("div");
-  div.innerText = `${data.from}: ${data.message}`;
-  document.getElementById("messages").appendChild(div);
-});
-
-// Typing
-socket.on("typing", (user) => {
-  document.getElementById("typing").innerText = user + " is typing...";
-  setTimeout(() => {
-    document.getElementById("typing").innerText = "";
-  }, 1000);
-});
-
-// Online users
-socket.on("online-users", (users) => {
-  const list = document.getElementById("users");
-  list.innerHTML = "";
-
-  Object.keys(users).forEach(id => {
-    const li = document.createElement("div");
-    li.innerText = users[id];
-    li.onclick = () => selectedUser = id;
-    list.appendChild(li);
-  });
+  div.innerText = msg;
+  messages.appendChild(div);
 });
