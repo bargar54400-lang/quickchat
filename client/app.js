24fcb5f676ef0d8d013fe0250c38f6952275a210
BarGar <bargar54400@gmail.com>
2026-04-08 20:53:25 -0500

    whatsapp ui

--- client/app.js
+++ client/app.js
@@ -2,18 +2,43 @@ const socket = io("https://quickchat-server-hap7.onrender.com");
 
 let username = "";
 let selectedUser = null;
+let selectedUserName = "";
 
+// JOIN
 function join() {
   username = document.getElementById("username").value;
   socket.emit("join", username);
 }
 
-// SEND MESSAGE (ENTER)
+// SELECT USER
+socket.on("online-users", (users) => {
+  const list = document.getElementById("users");
+  list.innerHTML = "";
+
+  Object.keys(users).forEach(id => {
+    if (users[id] !== username) {
+      const div = document.createElement("div");
+      div.innerText = users[id];
+
+      div.onclick = () => {
+        selectedUser = id;
+        selectedUserName = users[id];
+
+        document.getElementById("chatHeader").innerText =
+          selectedUserName;
+      };
+
+      list.appendChild(div);
+    }
+  });
+});
+
+// SEND MESSAGE
 document.getElementById("messageInput").addEventListener("keypress", (e) => {
   if (e.key === "Enter" && selectedUser) {
     const msg = e.target.value;
 
-    const msgDiv = addMessage(msg, "sent", "✔");
+    addMessage(msg, "sent");
 
     socket.emit("private-message", {
       to: selectedUser,
@@ -21,48 +46,19 @@ document.getElementById("messageInput").addEventListener("keypress", (e) => {
     });
 
     e.target.value = "";
-
-    // update to delivered after server response
-    socket.on("delivered", () => {
-      msgDiv.querySelector(".meta").innerText = "✔✔";
-    });
   }
 });
-
-// ADD MESSAGE UI
-function addMessage(msg, type, status = "") {
-  const div = document.createElement("div");
-  div.classList.add("message", type);
-
-  div.innerHTML = `
-    <div>${msg}</div>
-    <div class="meta">${status}</div>
-  `;
-
-  document.getElementById("messages").appendChild(div);
-  return div;
-}
 
 // RECEIVE MESSAGE
 socket.on("receive-message", (data) => {
   addMessage(data.message, "received");
 });
 
-// ONLINE USERS
-socket.on("online-users", (users) => {
-  const list = document.getElementById("users");
-  list.innerHTML = "";
+// ADD MESSAGE UI
+function addMessage(msg, type) {
+  const div = document.createElement("div");
+  div.classList.add("message", type);
+  div.innerText = msg;
 
-  Object.keys(users).forEach(id => {
-    const li = document.createElement("div");
-    li.innerText = users[id];
-    li.style.cursor = "pointer";
-
-    li.onclick = () => {
-      selectedUser = id;
-      alert("Chatting with " + users[id]);
-    };
-
-    list.appendChild(li);
-  });
-});
+  document.getElementById("messages").appendChild(div);
+}
