49538d5a456ebc536cd83697bd698c02c2e5ff51
BarGar <bargar54400@gmail.com>
2026-04-08 20:33:53 -0500

    fix simple chat

--- server/server.js
+++ server/server.js
@@ -1,47 +1,7 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
-
-let users = {};
-
-app.get("/", (req, res) => {
-  res.send("QuickChat Pro Running 🚀");
-});
-
 io.on("connection", (socket) => {
 
-  socket.on("join", (username) => {
-    users[socket.id] = username;
-    io.emit("online-users", users);
-  });
-
-  socket.on("private-message", ({ to, message }) => {
-    socket.to(to).emit("receive-message", {
-      message,
-      from: users[socket.id]
-    });
-  });
-
-  socket.on("typing", (to) => {
-    socket.to(to).emit("typing", users[socket.id]);
-  });
-
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-    io.emit("online-users", users);
+  socket.on("send-message", (msg) => {
+    io.emit("receive-message", msg);
   });
 
 });
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server running"));
